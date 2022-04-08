637d25da8635329b376aaf2235f1097f9d5145ae
dracero <diego.racero@gmail.com>
2022-04-08 16:28:59 -0300

    first image

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+import Camera from "./Camera";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Camera />
+    </div>
+  );
+}
---
+++ src/Camera.js
@@ -0,0 +1,42 @@
+import React, { useCallback, useRef } from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+
+const Camera = () => {
+  const webcamRef = useRef(null);
+  const [imageSrc, setImgSrc] = React.useState(null);
+
+  const capture = useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+    },
+    [webcamRef]
+  );
+  return (
+    <>
+      <Webcam
+        audio={false}
+        height={240}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={240}
+        videoConstraints={videoConstraints}
+      />
+    <button onClick={capture}>Capture photo</button>
+    {imageSrc && (
+        <img
+          src={imageSrc}
+          alt="trial file"
+        />
+      )}
+    </>
+  );
+};
+
+export default Camera;
